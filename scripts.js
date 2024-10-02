03eab09f1f10bef1a4c0565ee3c49cfa147c7a69
Doreen Nafula <113023805+Nafula-Doreen@users.noreply.github.com>
2024-10-02 17:38:57 +0300

    commit-one wanjiku

---
+++ scripts.js
@@ -0,0 +1,11 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+  const themeToggle = document.getElementById('theme-toggle');
+  const body = document.body;
+
+  themeToggle.addEventListener('click', function () {
+    body.classList.toggle('dark-mode');
+  });
+});
+
+
